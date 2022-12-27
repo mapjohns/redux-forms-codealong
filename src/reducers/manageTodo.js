e18baa6563c18a9df7a7d3d1789012fe165c1ef7
Matthew <mattj20@gmail.com>
2022-12-26 18:17:29 -0800

    commit tree 8ac7a964e6764046154ff0ae0a4dc7ca0ec0b4fe

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,6 +3,13 @@ export default function manageTodo(state = {
 }, action) {
 
   console.log("reducer received this action:", action);
+  switch (action.type) {
+    case "ADD_TODO":
+      console.log({ todos: [...state.todos, action.payload.text] });
 
-  return state;
+      return { todos: [...state.todos, action.payload.text] };
+
+      default:
+        return state;
+  }
 }
